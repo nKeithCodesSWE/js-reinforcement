b37f41339b6b4716eca27c0d340412b02c1fe2ba
nKeithCodesSWE <ngwerumekt@gmail.com>
2026-01-21 08:27:57 +1300

    styled counter app

--- state-2/index.js
+++ state-2/index.js
@@ -1,28 +1,29 @@
 // 1. State
 let count = 0;
-// 2. Elements
-const countEl = document.getElementById("count");
-const incBtn = document.getElementById("increment");
-const decBtn = document.getElementById("decrement");
 
-// 3. Render (UI) - One location for UI updates
-function render() {
+// 2. Gather Elements
+const countEl = document.getElementById('count');
+const incBtn = document.getElementById('increment');
+const decBtn = document.getElementById('decrement');
+
+// 3. Render UI
+function renderUI() {
     countEl.textContent = count;
-}
+} 
 
-// 4. Events 
-incBtn.addEventListener("click", () => {
+// 4. Events for buttons
+incBtn.addEventListener('click', () => {
     count = count + 1;
-    render();
-    console.log("add");
+    renderUI();
+    console.log('Count: ' + count);
 });
 
-decBtn.addEventListener("click", () => {
+decBtn.addEventListener('click', () => {
     if (count === 0) {
         return;
     } else {
         count = count - 1;
-        render();
-        console.log("subtract");
+        renderUI();
+        console.log('Count: ' + count);
     }
-})
+});
